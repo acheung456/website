ebb9ae45c6727484563b4e5d5a24a590b9ad1820
Andrew Cheung <andrew.cheung@twosigmaiq.com>
2018-07-29 15:16:32 -0400

    freezing

--- github-pages-site/app.py
+++ github-pages-site/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+app.config.from_pyfile('settings.py')
 
 @app.route("/")
-
 def index():
     return render_template('index.html')
 
